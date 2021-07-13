f403de2d58d826ac084128030ae6937dde905f7b
unknown <adelyaka.mitsk@gmail.com>
2021-07-13 09:22:10 +0300

    changes made on 12/07

---
+++ script.js
@@ -0,0 +1,19 @@
+// const taskDelete = document.querySelector('.delete-button')
+
+// taskDelete.addEventListener('click', (event) => {
+//     event.target.parentElement.remove
+// })
+
+const taskAdd = document.createElement('.input-field')
+taskAdd.addEventListener('click', (event) => {
+    parentElement.append
+})
+
+
+
+let changeSortButton = document.querySelector('.sort-button')
+
+let imageSortButton = document.querySelector('.grey-down')
+function changeSortButtons() {
+
+}
